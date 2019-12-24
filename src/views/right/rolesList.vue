8954422dfb67cf3aec6137ac870d20f2402532e8
蟑螂恶霸 <zhanglangeba@qq.com>
2019-12-24 14:03:53 +0800

    角色列表组件完成

--- src/views/right/rolesList.vue
+++ src/views/right/rolesList.vue
@@ -1,13 +1,57 @@
 <template>
-    <div class="rolesList">角色列表</div>
+  <div class="rolesList">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+      <el-breadcrumb-item>角色列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 按钮 -->
+    <el-button type="success">添加角色</el-button>
+    <!-- 表格 -->
+    <el-table :data="rolesList" border style="width: 100%;margin-top:10px">
+      <el-table-column type="expand">
+        <template slot-scope="">
+      
+        </template>
+      </el-table-column>
+      <el-table-column label="商品 ID" prop="id"></el-table-column>
+      <el-table-column type="index" width="50"></el-table-column>
+      <el-table-column prop="roleName" label="角色名称"></el-table-column>
+      <el-table-column prop="roleDesc" label="描述"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope>
+          <el-tooltip class="item" effect="dark" content="编辑" placement="top-start">
+            <el-button type="primary" icon="el-icon-edit" circle></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="授权" placement="top-start">
+            <el-button type="success" icon="el-icon-wind-power" circle></el-button>
+          </el-tooltip>
+          <el-tooltip class="item" effect="dark" content="删除" placement="top-start">
+            <el-button type="danger" icon="el-icon-delete" circle></el-button>
+          </el-tooltip>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
+import { getRoleList } from "@/api/roles_index.js";
 export default {
-    
-}
+  data() {
+    return {
+      rolesList: []
+    };
+  },
+  mounted() {
+    getRoleList().then(res => {
+      console.log(res);
+      this.rolesList = res.data.data;
+    });
+  }
+};
 </script>
 
 <style lang="less" scoped>
-
 </style>
